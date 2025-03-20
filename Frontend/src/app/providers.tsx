5a61e1cdec88e70bc66da115b013ab7bba011a05
Fuzyal234 <fuzyalahmed786@gmail.com>
2025-03-20 13:27:20 +0500

    fixed

--- Frontend/src/app/providers.tsx
+++ Frontend/src/app/providers.tsx
@@ -1,18 +1,10 @@
 'use client'
-import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </ChakraProvider>
-    </CacheProvider>
+    <ChakraProvider>
+      {children}
+    </ChakraProvider>
   )
 } 
